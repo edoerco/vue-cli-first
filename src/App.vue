26ab7b2f08dd375943e38febd745bf07113151c4
Edoardo <ercolino.edoardo@gmail.com>
2021-09-17 17:14:39 +0200

    primo commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Title />
+    <jumbotron />
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import jumbotron from './components/jumbotron.vue'
+import Title from './components/Title.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    jumbotron,
+    Title
+  },
+};
 </script>
 
 <style lang="scss">
@@ -22,7 +26,6 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/jumbotron.vue
@@ -0,0 +1,13 @@
+<template>
+    <img src="https://image.posterlounge.it/images/l/1894548.jpg" alt="superman">
+</template>
+
+<script>
+export default {
+    name: 'jumbotron',
+}
+</script>
+
+<style scoped lang="scss">
+   
+</style>
